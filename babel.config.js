00ee66eaa13cff42f2c84bfac66836da93e20550
NickoMaill <nicomaillols@gmail.com>
2023-03-12 14:24:23 +0100

    fix android build

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,27 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ['module:metro-react-native-babel-preset'],
-        plugins: [
-            [
-                'module:react-native-dotenv',
-                {
-                    envName: 'APP_ENV',
-                    moduleName: '@env',
-                    path: '.env.development.local',
-                    safe: false,
-                    allowUndefined: true,
-                    verbose: false,
-                },
-            ],
-            [
-                'module-resolver',
-                {
-                    alias: {
-                        '~': './src',
-                    },
-                },
-            ],
-        ],
-    };
+  api.cache(true);
+  return {
+      presets: ['module:metro-react-native-babel-preset'],
+      plugins: [
+          [
+              'module:react-native-dotenv',
+              {
+                  envName: 'APP_ENV',
+                  moduleName: '@env',
+                  path: '.env.development.local',
+                  safe: false,
+                  allowUndefined: true,
+                  verbose: false,
+              },
+          ],
+          [
+              'module-resolver',
+              {
+                  alias: {
+                      '~': './src',
+                  },
+              },
+          ],
+      ],
+  };
 };
